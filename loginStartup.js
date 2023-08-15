98b56607dac5f5e019f81a64c13b4be84b019b2d
TistaChowdhury <tistacho.slsn11abcd@gmail.com>
2023-08-15 12:37:39 +0530

    entreprenuers list updated

--- loginStartup.js
+++ loginStartup.js
@@ -8,8 +8,10 @@ loginForm.addEventListener('submit', function(event) {
     name: formData.get('name'),
     contact: formData.get('contact'),
     email: formData.get('email'),
+    startupIdea: formData.get('startupIdea'), 
     password: formData.get('password'),
     resume: formData.get('resume'),
+
   };
   users.push(user); // Add the user to the array
   console.log("User added:", user);
--- userList.js
+++ userList.js
@@ -1,2 +1,32 @@
+const users = JSON.parse(localStorage.getItem('users') || '[]');
+const userCardsContainer = document.getElementById('userCards');
 
-  
+for (const user of users) {
+  const userCard = createUserCard(user);
+  userCardsContainer.appendChild(userCard);
+}
+
+function createUserCard(user) {
+  const userCard = document.createElement('div');
+  userCard.className = 'user-card';
+
+  const nameElement = document.createElement('h3');
+  nameElement.textContent = user.name;
+
+  const startupIdeaElement = document.createElement('p');
+  startupIdeaElement.textContent = `Startup Idea: ${user.startupIdea}`;
+
+  const emailElement = document.createElement('p');
+  emailElement.textContent = `Email: ${user.email}`;
+
+  const findAskLink = document.createElement('a');
+  findAskLink.href = '#'; // Replace '#' with the actual link URL
+  findAskLink.textContent = 'Find their ask >>';
+
+  userCard.appendChild(nameElement);
+  userCard.appendChild(startupIdeaElement);
+  userCard.appendChild(emailElement);
+  userCard.appendChild(findAskLink);
+
+  return userCard;
+}
